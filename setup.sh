4a18cfe27b492477c1c7cb6d7e29ec7db6e62880
Jeremy Groh <jgroh9@users.noreply.github.com>
2024-01-16 08:28:25 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -32,8 +32,5 @@ ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
-echo "Applying changes to .zshrc"
-omz reload
-
 echo "Reload the .zshrc file"
 exec zsh
